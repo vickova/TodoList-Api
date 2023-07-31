36008d12fc1327f5e5d8843438d5fa79bdf3a6a5
Olumide Victoria <olumideronke@gmail.com>
2023-07-30 20:02:15 -0700

    AccessControl2

--- middleware/access-control.js
+++ middleware/access-control.js
@@ -1,5 +1,5 @@
 const AccessControl = (req, res, next) => {
-    res.setHeader("Access-Control-Allow-Origin", "https://yoursite.com");
+    res.setHeader("Access-Control-Allow-Origin", "https://todo-list-api-8vwz.onrender.com/");
     res.header(
       "Access-Control-Allow-Headers",
       "Origin, X-Requested-With, Content-Type, Accept"
